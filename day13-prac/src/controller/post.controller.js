0434145ef30f9701a119804792fbbacd34b750fb
RajanPal920 <palraja842@gmail.com>
2026-02-14 22:54:13 +0530

    implement get and get all post for a user

--- day13-prac/src/controller/post.controller.js
+++ day13-prac/src/controller/post.controller.js
@@ -42,6 +42,82 @@ async function createPostController(req, res) {
   });
 }
 
+async function getPostController(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized access",
+    });
+  }
+
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    res.status(401).json({
+      message: "Unauthorized, Token invalid",
+    });
+  }
+
+  const userId = decoded.id;
+
+  const posts = await postModel.find({
+    user: userId,
+  });
+
+  res.status(200).json({
+    message: "Post fetched sucessfully",
+    posts,
+  });
+}
+
+async function getPostDetailsController(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized access",
+    });
+  }
+
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+
+  const userId = decoded.id;
+  const postId = req.params.postId;
+
+  const post = await postModel.findById(postId);
+
+  if (!post) {
+    return res.status(404).json({
+      message: "Post Not Found",
+    });
+  }
+
+  const isValidUser = post.user.toString() === userId;
+
+  if (!isValidUser) {
+    return res.status(403).json({
+      message: "Forbidden Content",
+    });
+  }
+
+  return res.status(200).json({
+    message: "Post Fetched Successfully",
+    post,
+  });
+}
+
 module.exports = {
   createPostController,
+  getPostController,
+  getPostDetailsController,
 };
